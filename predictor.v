e063b69d5d88eeaebf20f7237f27334115bcbae9
Ali Shahali <67241070+alishahali1382@users.noreply.github.com>
2023-06-01 10:58:56 +0330

    Update predictor.v

--- predictor.v
+++ predictor.v
@@ -1,89 +1,28 @@
-module predictor(input wire request, result, clk, taken, output reg prediction);
+module predictor(
+	input wire request,
+	input wire result,
+	input wire clk,
+	input wire taken,
+	output reg prediction
+);
 
-  // Your code
-  reg[1:0] nextState = 2'b11;
-  
-  always @(posedge clk) begin
-    if(request == 1) begin
-      prediction = nextState[1];
-    end
-    
-    if(result == 1) begin
-      if(nextState == 2'b00) begin
-        if(taken == 0) begin
-          nextState = 2'b00;
-        end
-        else begin
-          nextState = 2'b01;
-        end
-      end
-      else if(nextState == 2'b01) begin 
-        if(taken == 0) begin
-          nextState = 2'b00;
-        end
-        else begin
-          nextState = 2'b10;
-        end
-      end
-      else if(nextState == 2'b10) begin 
-        if(taken == 0) begin
-          nextState = 2'b01;
-        end
-        else begin
-          nextState = 2'b11;
-        end
-      end
-      else begin 
-        if(taken == 0) begin
-          nextState = 2'b10;
-        end
-        else begin
-          nextState = 2'b11;
-        end
-      end
-    end
-  end
+reg [1:0] counter=3;
+
+always @(posedge clk) begin
+	if (result) begin
+		if (taken) begin
+			if (counter < 2'b11) begin
+				counter = counter + 1;
+			end
+		end else begin
+			if (counter > 2'b00) begin
+				counter = counter - 1;
+			end
+		end
+	end
+	if (request) begin
+		prediction = counter[1];
+	end
+end
 
 endmodule
-
-module counter(input wire[1:0] a, wire s, output wire result);
-  reg[1:0] r;
-  
-  always@(a or s) begin
-    if(a == 2'b00) begin
-      if(s == 0) begin
-        r = 2'b00;
-      end
-      else begin
-        r = 2'b01;
-      end
-    end
-    else if(a == 2'b01) begin 
-      if(s == 0) begin
-        r = 2'b00;
-      end
-      else begin
-        r = 2'b10;
-      end
-    end
-    else if(a == 2'b10) begin 
-      if(s == 0) begin
-        r = 2'b01;
-      end
-      else begin
-        r = 2'b11;
-      end
-    end
-    else begin 
-      if(s == 0) begin
-        r = 2'b10;
-      end
-      else begin
-        r = 2'b11;
-      end
-    end
-  end
-  
-  assign result = r[1];
-  
-endmodule  
