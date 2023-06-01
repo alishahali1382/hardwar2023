e6b4414ee8539ca4a2eab990187285218037bbf0
alishahali1382 <alishahali1382@gmail.com>
2023-06-01 10:47:30 +0330

    update

--- predictor.v
+++ predictor.v
@@ -1,28 +1,28 @@
 module predictor(
-    input wire request,
-    input wire result,
-    input wire clk,
-    input wire taken,
-    output reg prediction
+	input wire request,
+	input wire result,
+	input wire clk,
+	input wire taken,
+	output reg prediction
 );
 
 reg [1:0] counter;
 
 always @(posedge clk) begin
-    if (request) begin
-        prediction <= (counter > 1'b1);
-    end
-    if (result) begin
-        if (taken) begin
-            if (counter < 2'b11) begin
-                counter <= counter + 1;
-            end
-        end else begin
-            if (counter > 2'b00) begin
-                counter <= counter - 1;
-            end
-        end
-    end
+	if (result) begin
+		if (taken) begin
+			if (counter < 2'b11) begin
+				counter = counter + 1;
+			end
+		end else begin
+			if (counter > 2'b00) begin
+				counter = counter - 1;
+			end
+		end
+	end
+	if (request) begin
+		prediction = (counter > 1'b1);
+	end
 end
 
 endmodule
