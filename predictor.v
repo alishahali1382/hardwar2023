9366c8a3e462783158bbecc9422bd3a7ac23be10
alishahali1382 <alishahali1382@gmail.com>
2023-06-01 10:44:06 +0330

    submit

--- predictor.v
+++ predictor.v
@@ -1,5 +1,28 @@
-module predictor(input wire request, result, clk, taken, output reg prediction);
+module predictor(
+    input wire request,
+    input wire result,
+    input wire clk,
+    input wire taken,
+    output reg prediction
+);
 
-// Your code
+reg [1:0] counter;
+
+always @(posedge clk) begin
+    if (request) begin
+        prediction <= (counter > 1'b1);
+    end
+    if (result) begin
+        if (taken) begin
+            if (counter < 2'b11) begin
+                counter <= counter + 1;
+            end
+        end else begin
+            if (counter > 2'b00) begin
+                counter <= counter - 1;
+            end
+        end
+    end
+end
 
 endmodule
